a6be47fb7e15995816e1da105d5f0797ba4c0a15
Joaquin Galvan <joaquingd1@hotmail.com>
2024-11-05 23:55:10 -0500

    Feat: Añadir navbar para el creador

--- src/app/pages/creator/creator-layout/creator-layout.component.ts
+++ src/app/pages/creator/creator-layout/creator-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PublicacionFormComponent } from '../publicacion-form/publicacion-form.component';
+import { NavbarCreatorComponent } from "../../../shared/components/navbar-creator/navbar-creator.component";
 
 @Component({
   selector: 'app-creator-layout',
   standalone: true,
-  imports: [RouterLink,PublicacionFormComponent],
+  imports: [RouterLink, PublicacionFormComponent, NavbarCreatorComponent],
   templateUrl: './creator-layout.component.html',
   styleUrl: './creator-layout.component.css'
 })
