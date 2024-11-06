a05314da1e0d8bfff776ba32900d73bddbf413b8
Joaquin Galvan <joaquingd1@hotmail.com>
2024-11-05 22:50:19 -0500

    Configuración base del feature, más la aplicación de routes links y un formulario básico

--- src/app/pages/creator/creator-layout/creator-layout.component.ts
+++ src/app/pages/creator/creator-layout/creator-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { PublicacionFormComponent } from '../publicacion-form/publicacion-form.component';
 
 @Component({
   selector: 'app-creator-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,PublicacionFormComponent],
   templateUrl: './creator-layout.component.html',
   styleUrl: './creator-layout.component.css'
 })
---
+++ src/app/pages/creator/publicacion-form/publicacion-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-publicacion-form',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './publicacion-form.component.html',
+  styleUrl: './publicacion-form.component.css'
+})
+export class PublicacionFormComponent {
+
+}
